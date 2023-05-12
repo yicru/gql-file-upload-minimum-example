6b62ba568864acbf759d5a24563f3f3555ed52c9
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-05-12 14:47:41 +0900

    feat(package.json): add urql dependency to enable GraphQL queries in the app feat(_app.tsx): add urql client to provide GraphQL queries to the app through the /api/graphql endpoint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import type { AppProps } from 'next/app'
 
+import { Client, cacheExchange, fetchExchange, Provider } from 'urql'
+
+const client = new Client({
+  url: '/api/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Provider value={client}>
+        <Component {...pageProps} />
+      </Provider>
     </ChakraProvider>
   )
 }
